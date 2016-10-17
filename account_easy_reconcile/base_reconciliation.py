3405823fb434dffcfeac74f29970ca9044639066
Manu <manuel.reyes@openpyme.mx>
2016-10-17 13:27:01 -0500

    fix(account_easy_reconcile/base_reconciliation.py): Create the relation to load the easy_reconcile m
    add the relation in base_reconciliation.py file allowed load properly easy_reconcile module

--- account_easy_reconcile/base_reconciliation.py
+++ account_easy_reconcile/base_reconciliation.py
@@ -35,7 +35,9 @@ class EasyReconcileBase(orm.AbstractModel):
         'account_id': fields.many2one(
             'account.account', 'Account', required=True),
         'partner_ids': fields.many2many(
-            'res.partner', string="Restrict on partners"),
+            'res.partner',
+            relation='easy_reconcile_simple_res_partner_rel',
+            string="Restrict on partners"),
         # other columns are inherited from easy.reconcile.options
     }
 
